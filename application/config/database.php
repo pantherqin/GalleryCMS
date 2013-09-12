055b4e5f0da898ef8461b188cc28a23fbb9acdbe
pantherqin <pantherqin@messymessy.(none)>
2013-09-12 15:54:30 +0800

    update gitignore

--- application/config/database.php
+++ application/config/database.php
@@ -50,8 +50,8 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'root';
-$db['default']['database'] = 'gallerycms';
+$db['default']['password'] = 'password';#'root';
+$db['default']['database'] = 'my_gallerycms';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
